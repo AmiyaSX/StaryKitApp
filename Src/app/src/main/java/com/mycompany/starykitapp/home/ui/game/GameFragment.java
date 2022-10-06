9fc5700cbf94602ce23917256fd9cc1fd3ae13a6
jxn <2416692948@qq.com>
2022-10-06 16:05:18 +0800

    完善游戏区文件结构及与其相关的导航文件，新创错题区包

---
+++ Src/app/src/main/java/com/mycompany/starykitapp/home/ui/game/GameFragment.java
@@ -0,0 +1,42 @@
+package com.mycompany.starykitapp.home.ui.game;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mycompany.starykitapp.R;
+import com.mycompany.starykitapp.databinding.FragmentGameBinding;
+import com.mycompany.starykitapp.databinding.FragmentHomeBinding;
+import com.mycompany.starykitapp.home.ui.home.HomeViewModel;
+
+public class GameFragment extends Fragment {
+
+    private FragmentGameBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        GameViewModel gameViewModel =
+                new ViewModelProvider(this).get(GameViewModel.class);
+
+        binding = FragmentGameBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
