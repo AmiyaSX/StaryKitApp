482ce4167204217a8117c9b089e42f9b9f0a9468
cuilijuan <2304830283@qq.com>
2022-10-09 19:52:53 +0800

    我的故事区ui完成u以及文件夹创建

---
+++ Src/app/src/main/java/com/mycompany/starykitapp/home/ui/mystory/MystoryFragment.java
@@ -0,0 +1,38 @@
+package com.mycompany.starykitapp.home.ui.mystory;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mycompany.starykitapp.R;
+
+public class MystoryFragment extends Fragment {
+
+    private MystoryViewModel mViewModel;
+
+    public static MystoryFragment newInstance() {
+        return new MystoryFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mystory, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MystoryViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
