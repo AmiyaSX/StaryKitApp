2b6a6afa9a97005c45a300cb3e0ea2fc04ec921e
jxn <2416692948@qq.com>
2022-10-17 19:54:00 +0800

    我的错题本界面完善+我的故事相应故事跳转查找

--- Src/app/src/main/java/com/mycompany/starykitapp/home/ui/mystory/MyStoryFragment.java
+++ Src/app/src/main/java/com/mycompany/starykitapp/home/ui/mystory/MyStoryFragment.java
@@ -2,6 +2,7 @@ package com.mycompany.starykitapp.home.ui.mystory;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,10 +14,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.mycompany.starykitapp.R;
+import com.mycompany.starykitapp.WebViewActivity;
+import com.mycompany.starykitapp.config.StoryLink;
+import com.mycompany.starykitapp.databinding.FragmentMyStoryBinding;
+import com.mycompany.starykitapp.home.ui.mystory.MyStoryViewModel;
+
 
 public class MyStoryFragment extends Fragment {
 
     private MyStoryViewModel mViewModel;
+    private FragmentMyStoryBinding binding;
 
     public static MyStoryFragment newInstance() {
         return new MyStoryFragment();
@@ -26,6 +33,26 @@ public class MyStoryFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_my_story, container, false);
+    }
+
+    private void initEvent() {
+        Intent intent = new Intent(requireActivity(), WebViewActivity.class);
+        binding.mystoryCard1.setOnClickListener(v ->{
+            intent.putExtra("url", StoryLink.STORY_YIQIANLINGYI_URL);
+        });
+        binding.mystoryCard2.setOnClickListener(v -> {
+            intent.putExtra("url", StoryLink.STORY_BAOHULU_URL);
+            startActivity(intent);
+        });
+        binding.mystudyCard3.setOnClickListener(v -> {
+            intent.putExtra("url", StoryLink.STORY_DAOCAOREN_URL);
+            startActivity(intent);
+        });
+        binding.mystoryCard4.setOnClickListener(v -> {
+            intent.putExtra("url", StoryLink.STORY_XIAOWANGZI_URL);
+            startActivity(intent);
+        });
+
     }
 
     @Override
